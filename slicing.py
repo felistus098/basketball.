913f2de914b8a315d9adb7a90ea87f42335c0f0c
felistus098 <57259896+felistus098@users.noreply.github.com>
2021-10-20 13:20:14 +0300

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 12:58:36 2021
+
+@author: Felistus
+"""
+#allocating values to areas
+areas=["bedroom",11.5,"living",23.9,"bathroom",25.0,"wash",26.9,"dining",29.0]
+
+#allocating the first six values from areas to downstairs
+ downstairs=areas[0:6]
+ 
+ #allocating the last four values from areas to upstairs
+ 
+ upstairs =areas[6:10]
+ #print downstairs and upstairs
+ print(downstairs)
+ 
+
